5c857d51b9f44d882d2b3b7b997c7ef0d9a957bc
pudge <cgrvel@gmail.com>
2021-11-16 21:56:17 +0530

    travis-aws-deploy-3

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,19 @@ import Fib from './Fib'
 function App() {
   return (
     <Router>
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <div className="App"> */}
+    <div>
+      {/* <header className="App-header"> */}
+      <header>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1 className="App-title"> Fib Calculator </h1>
         <Link to="/">Home</Link>
         <Link to="otherPage">OtherPage</Link>
       </header>
       <div>
           <Route exact path="/" component={Fib}></Route>
           <Route exact path="/otherPage" component={OtherPage}></Route>
-        </div>
+      </div>
     </div>
     </Router>
   );
